2b38e417d954a778aa389e30f0445385044ed605
ppenkof <overton@abv.bg>
2025-11-16 18:36:45 +0200

    Add proper details link

--- client/src/components/game/Game.jsx
+++ client/src/components/game/Game.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router";
+
 export default function Game({
-    // _id,
+    _id,
     title,
     genre,
     imageUrl
@@ -10,8 +12,8 @@ export default function Game({
             <div className="details-overlay">
                 <p className="name">{title}</p>
                 <p className="genre">{genre}</p>
-                <a href="#" className="details-button">Details</a>
-            </div>
+                <Link to={`/games/${_id}/details`} className="details-button">Details</Link>
+         </div>
         </div>
     );
 }
