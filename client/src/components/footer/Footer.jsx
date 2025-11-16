1c4aeb2906c870d013514a5b99df27f0a6d0118c
ppenkof <overton@abv.bg>
2025-11-16 16:06:24 +0200

    Extract components Header, Home and  Footer

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                <p class="center-text">React Course Exercise @ SoftUni &trade;</p>
+            </footer>
+        </>
+    );
+}
