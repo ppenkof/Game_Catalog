4fbaab8186ffd2f5ade267fe55aeb11a1831b18b
ppenkof <overton@abv.bg>
2025-12-06 15:27:42 +0200

    Refactor GameCreate and save old one in the same folder

---
+++ client/src/components/game-create/GameCreate-ImageUpload.jsx
@@ -0,0 +1,117 @@
+import { useNavigate } from "react-router";
+import request from "../../utils/request";
+import { useEffect, useState } from "react";
+import {getDownloadURL, ref, uploadBytes} from 'firebase/storage';
+import { storage } from "../../firebase";
+
+export default function GameCreate() {
+    const navigate = useNavigate();
+    const [imageUpload, setImageUpload] = useState(false);
+    const [imagePreview, setImagePreview] = useState(null);
+
+    useEffect(() => {
+        return () => {
+            URL.revokeObjectURL(imagePreview);
+            setImagePreview(null);
+        };
+    }, [imageUpload]);
+
+    const createGameHandler = async (e) => {
+        e.preventDefault();
+        
+        const formData = new FormData(e.target);
+        const {image, ...data} = Object.fromEntries(formData);
+
+        if (imageUpload) {
+            const imageRef = ref(storage, `images/${image.name}`);
+            uploadBytes(imageRef, image)
+            data.imageUrl = await getDownloadURL(imageRef).then((url) => {
+                data.imageUrl = url;
+            });
+        } else {
+          data.imageUrl = image;
+        }
+
+        data.players=Number(data.players);
+        data._createdOn=Date.now();
+
+        // It is commented because use custom Request function
+        // const response = await fetch('http://localhost:3030/jsonstore/games', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(data)
+        // });
+
+        //const result = await response.json();
+        const result = await request('/games', 'POST', data);
+        console.log(result);
+        
+        navigate('/games');
+    }
+
+    // State to toggle between image url and image upload
+    const imageChangeHandler = (e) => {
+        const image = e.target.files[0];
+        const imageUrl = URL.createObjectURL(image);
+
+        setImagePreview(imageUrl);
+    }
+
+    const imageUploadClickHandler = () => {
+        setImageUpload(state => !state);
+    }
+
+    return (
+        <section id="add-page">
+        <form id="add-new-game" onSubmit={createGameHandler}>
+            <div className="container">
+
+                <h1>Add New Game</h1>
+
+                <div className="form-group-half">
+                    <label htmlFor="gameName">Game Name:</label>
+                    <input type="text" id="gameName" name="title" placeholder="Enter game title..."/>
+                </div>
+
+                <div className="form-group-half">
+                    <label htmlFor="genre">Genre:</label>
+                    <input type="text" id="genre" name="genre" placeholder="Enter game genre..."/>
+                </div>
+
+                <div className="form-group-half">
+                    <label htmlFor="activePlayers">Active Players:</label>
+                    <input type="number" id="activePlayers" name="players" min="0" placeholder="0"/>
+                </div>
+
+                <div className="form-group-half">
+                    <label htmlFor="releaseDate">Release Date:</label>
+                    <input type="date" id="releaseDate" name="date"/>
+                </div>
+
+                {/* // State to toggle between image url and image upload */}
+                <div className="form-group-full">
+                    <label htmlFor="imageUrl">{imageUpload ? 'Image Upload': 'Image Url'}</label>
+                    <button type="button" className="details-button" onClick = {imageUploadClickHandler}>{imageUpload ? 'Image Url': 'Image Upload'}</button>
+                    {imageUpload 
+                        ? <input type="file" id="image" name="image" placeholder="Upload file..." onChange={imageChangeHandler}/>
+                        : <input type="text" id="image" name="image" placeholder="Enter image URL..."/>
+                    }
+
+                    {imagePreview && 
+                    <img className="image-preview" src={imagePreview} alt="preview-image"/>
+                    }
+                </div>
+
+                <div className="form-group-full">
+                    <label htmlFor="summary">Summary:</label>
+                    <textarea name="summary" id="summary" rows="5" placeholder="Write a brief summary..."></textarea>
+                </div>
+
+                <input className="btn submit" type="submit" value="ADD GAME"/>
+            </div>
+        </form>
+    </section>
+    );
+}
--- client/src/components/game-create/GameCreate.jsx
+++ client/src/components/game-create/GameCreate.jsx
@@ -1,40 +1,18 @@
 import { useNavigate } from "react-router";
-import request from "../../utils/request";
-import { useEffect, useState } from "react";
-import {getDownloadURL, ref, uploadBytes} from 'firebase/storage';
-import { storage } from "../../firebase";
+import useForm from "../../hooks/useForm";
+import useRequest from "../../hooks/useRequest";
 
 export default function GameCreate() {
     const navigate = useNavigate();
-    const [imageUpload, setImageUpload] = useState(false);
-    const [imagePreview, setImagePreview] = useState(null);
+    const {request} = useRequest();
 
-    useEffect(() => {
-        return () => {
-            URL.revokeObjectURL(imagePreview);
-            setImagePreview(null);
-        };
-    }, [imageUpload]);
-
-    const createGameHandler = async (e) => {
-        e.preventDefault();
-        
-        const formData = new FormData(e.target);
-        const {image, ...data} = Object.fromEntries(formData);
-
-        if (imageUpload) {
-            const imageRef = ref(storage, `images/${image.name}`);
-            uploadBytes(imageRef, image)
-            data.imageUrl = await getDownloadURL(imageRef).then((url) => {
-                data.imageUrl = url;
-            });
-        } else {
-            data.imageUrl = image;
-        }
+    const createGameHandler = async (values) => {
+        const data = values;
 
         data.players=Number(data.players);
         data._createdOn=Date.now();
 
+        // it is comment because use custom request function
         // const response = await fetch('http://localhost:3030/jsonstore/games', {
         //     method: 'POST',
         //     headers: {
@@ -44,67 +22,62 @@ export default function GameCreate() {
         // });
 
         // const result = await response.json();
-        const result = await request('/games', 'POST', data);
-        console.log(result);
+        try {
+            await request('/data/games', 'POST', data);
+            navigate('/games');
+        } catch (error) {
+            alert(error.message);
+        }
         
-        navigate('/games');
     }
 
-    const imageChangeHandler = (e) => {
-        const image = e.target.files[0];
-        const imageUrl = URL.createObjectURL(image);
-
-        setImagePreview(imageUrl);
-    }
-
-    const imageUploadClickHandler = () => {
-        setImageUpload(state => !state);
-    }
+    const {
+        register,
+        formAction
+    } = useForm(createGameHandler, {
+        title: '', 
+        genre: '', 
+        players: 0, 
+        date: '', 
+        imageUrl: '', 
+        summary: ''
+    });
 
     return (
         <section id="add-page">
-        <form id="add-new-game" onSubmit={createGameHandler}>
+        <form id="add-new-game" action = {formAction}>
             <div className="container">
 
                 <h1>Add New Game</h1>
 
                 <div className="form-group-half">
                     <label htmlFor="gameName">Game Name:</label>
-                    <input type="text" id="gameName" name="title" placeholder="Enter game title..."/>
+                    <input type="text" id="gameName" {...register('title')} placeholder="Enter game title..."/>
                 </div>
 
                 <div className="form-group-half">
                     <label htmlFor="genre">Genre:</label>
-                    <input type="text" id="genre" name="genre" placeholder="Enter game genre..."/>
+                    <input type="text" id="genre" {...register('genre')} placeholder="Enter game genre..."/>
                 </div>
 
                 <div className="form-group-half">
                     <label htmlFor="activePlayers">Active Players:</label>
-                    <input type="number" id="activePlayers" name="players" min="0" placeholder="0"/>
+                    <input type="number" id="activePlayers" {...register('players')} min="0" placeholder="0"/>
                 </div>
 
                 <div className="form-group-half">
                     <label htmlFor="releaseDate">Release Date:</label>
-                    <input type="date" id="releaseDate" name="date"/>
+                    <input type="date" id="releaseDate" {...register('date')}/>
                 </div>
 
                 <div className="form-group-full">
-                    <label htmlFor="imageUrl">{imageUpload ? 'Image Upload': 'Image Url'}</label>
-                    <button type="button" className="details-button" onClick = {imageUploadClickHandler}>{imageUpload ? 'Image Url': 'Image Upload'}</button>
-                    {imageUpload 
-                        ? <input type="file" id="image" name="image" placeholder="Upload file..." onChange={imageChangeHandler}/>
-                        : <input type="text" id="image" name="image" placeholder="Enter image URL..."/>
-                    }
-
-                    {imagePreview && 
-                    <img className="image-preview" src={imagePreview} alt="preview-image"/>
-                    }
-
+                    <label htmlFor="imageUrl">Image Url:</label>
+                    <input type="text" id="image" {...register('imageUrl')} placeholder="Enter image URL..."/>
                 </div>
 
                 <div className="form-group-full">
                     <label htmlFor="summary">Summary:</label>
-                    <textarea name="summary" id="summary" rows="5" placeholder="Write a brief summary..."></textarea>
+                    <textarea {...register('summary')} id="summary" rows="5" placeholder="Write a brief summary..."></textarea>
                 </div>
 
                 <input className="btn submit" type="submit" value="ADD GAME"/>
